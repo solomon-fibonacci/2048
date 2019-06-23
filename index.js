ec90917ee52245a668e4b059fd85e15dd181d1b4
Solomon Omojola <omojolasolomon@gmail.com>
2019-06-23 18:54:23 +0200

    WIP: Major Refactor .. plus .. GameUI

--- index.js
+++ index.js
@@ -1,217 +1,209 @@
 export class Board {
-    constructor(n) {
-        this.coordinates = {};
-        this.emptyCoordinates = {};
-        this.validMoves = ["up", "right", "down", "left"];
-        this.size = n;
-        this.score = 0;
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                this.emptyCoordinates[`${i},${j}`] = {
-                    key: `${i},${j}`,
-                    value: undefined
-                };
+  constructor(n, callback) {
+    this.coordinates = {}
+    this.emptyCoordinates = {}
+    this.validMoves = ['up', 'right', 'down', 'left']
+    this.gameOverCallback = callback
+    this.size = n
+    this.score = 0
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        const coord = this.getCoord({row: i, column: j})
+        this.emptyCoordinates[coord] = {
+          key: coord,
+          value: 0,
+        }
+      }
+    }
+    this.addNewTile(2)
+  }
+
+  getCoord(rowColumn) {
+    const {row, column} = rowColumn
+    return JSON.stringify({row, column})
+  }
+
+  getCurrentCoord(i, j, direction) {
+    if (this.isVertical(direction)) {
+      return this.getCoord({row: j, column: i})
+    }
+    return this.getCoord({row: i, column: j})
+  }
+
+  addNewTile(n = 1) {
+    while (n) {
+      const keys = Object.keys(this.emptyCoordinates)
+      if (!keys.length) {
+        this.gameOver()
+      }
+      const randomKey = keys[Math.floor(Math.random() * keys.length)]
+      this.coordinates[randomKey] = {
+        key: randomKey,
+
+        value: 2,
+        isDerived: false,
+      }
+      delete this.emptyCoordinates[randomKey]
+      n--
+    }
+
+  gameOver() {
+    this.gameOverCallback(this)
+  }
+
+  getInitialWall(direction) {
+    if (direction === 'up' || direction === 'left') {
+      return 0
+    } else {
+      return this.size - 1
+    }
+  }
+
+  adjustIndexForDirection(y, x, direction) {
+    if (direction === 'down' || direction === 'right') {
+      return {i: this.size - 1 - y, j: this.size - 1 - x}
+    } else {
+      return {i: y, j: x}
+    }
+  }
+
+  move(direction) {
+    if (!this.validMoves.includes(direction)) {
+      throw new Error('Invalid move')
+    }
+
+    for (let y = 0; y < this.size; y++) {
+      let wall = this.getInitialWall(direction)
+      for (let x = 0; x < this.size; x++) {
+        const {i, j} = this.adjustIndexForDirection(y, x, direction)
+        const currentCoord = this.getCurrentCoord(i, j, direction)
+        const tile = this.coordinates[currentCoord]
+        const lastTile = this.getLastTile(i, wall, direction)
+
+        if (tile) {
+          if (this.shouldNotMove(tile, lastTile, wall, direction)) {
+            wall = this.moveWall(wall)
+          } else {
+            if (this.shouldCollapse(tile, lastTile)) {
+              this.collapse(tile, lastTile)
+            } else {
+              this.pushToWall(tile, wall, direction)
             }
+          }
         }
-        this.addNewTile(2);
+      }
     }
 
-    addNewTile(n = 1) {
-        while (n) {
-            let keys = Object.keys(this.emptyCoordinates);
-            let randomKey = keys[Math.floor(Math.random() * keys.length)];
-            this.coordinates[randomKey] = {
-                key: randomKey,
-                value: 2,
-                isDerived: false
-            };
-            delete this.emptyCoordinates[randomKey];
-            n--;
-        }
+    this.addNewTile()
+    return {occupied: this.coordinates, empty: this.emptyCoordinates}
+  }
+
+  shouldNotMove(tile, lastTile, wall, direction) {
+    if (
+      (!lastTile && this.isAtWall(tile, wall, direction)) ||
+      (lastTile &&
+        this.isAdj(tile, lastTile) &&
+        !this.shouldCollapse(tile, lastTile))
+    ) {
+      return true
     }
+    return false
+  }
 
-    move(direction) {
-        if (!this.validMoves.includes(direction)) {
-            return false;
-        }
+  isAdj(tile1, tile2) {
+    const row1 = this.getRow(tile1)
+    const row2 = this.getRow(tile2)
+    const column1 = this.getColumn(tile1)
+    const column2 = this.getColumn(tile2)
+    return (
+      (row1 === row2 && Math.abs(column1 - column2) === 1) ||
+      (column1 === column2 && Math.abs(row1 - row2))
+    )
+  }
 
-        switch (direction) {
-            case "up":
-                let ceiling = { index: "0,0", value: null, isDerived: false };
-                const isCeiling = coord => {
-                    return ceiling.index === coord;
-                };
-                const moveTileToTop = coord => {
-                    if (!ceiling.includes(",0")) {
-                        if (
-                            ceiling.value === this.coordinates[coord].value &&
-                            !ceiling.isDerived
-                        ) {
-                            this.coordinates[ceiling.index] = {
-                                key: ceiling.index,
-                                value: ceiling.value * 2
-                            };
-                            ceiling.isDerived = true;
-                            this.removeTile(coord);
-                        }
-                    }
-                    ceiling = { index: coord, value: this.coordinates[coord].value };
-                    this.removeTile(coord);
-                };
-
-                for (let i = 0; i < this.size; i++) {
-                    for (let j = 0; j < this.size; j++) {
-                        let currentCoord = `${i},${j}`;
-                        if (
-                            this.emptyCoordinates[currentCoord] ||
-                            isCeiling(currentCoord)
-                        ) {
-                        } else if (this.coordinates[currentCoord]) {
-                            moveTileToTop(currentCoord);
-                        }
-                    }
-                }
-                break;
-            case "left":
-                const isAtWall = (tile, wall) => {
-                    return wall === tile.key;
-                };
-                const pushToWall = (tile, wall) => {
-                    if (isAtWall(tile, wall)) {
-                        throw new Error(
-                            "Cant push tile to wall since it's already at wall"
-                        );
-                    } else {
-                        this.removeTile(tile.key);
-                        this.coordinates[
-                            tile.key.replace(/[0-9]*(?:.(?![0-9]))+$/gi, `${wall}`)
-                        ] = tile;
-                    }
-                };
-                const collapse = (sourceTile, destTile) => {
-                    this.removeTile(sourceTile.key);
-                    this.coordinates[destTile.key] *= 2;
-                };
-
-                for (let i = 0; i < this.size; i++) {
-                    let wall = 0;
-                    for (let j = 0; j < this.size; j++) {
-                        const currentCoord = `${i},${j}`;
-                        const tile = this.coordinates[currentCoord];
-                        const lastTile = wall && {
-                            key: `${i},${wall}`,
-                            value: this.coordinates[`${i},${wall}`],
-                            isDerived: false
-                        };
-
-                        if (tile) {
-                            if (isAtWall(tile, wall)) {
-                                if (
-                                    wall === 0 ||
-                                    lastTile.value !== tile.value ||
-                                    lastTile.isDerived
-                                )
-                                    wall++;
-                                else if (lastTile.value === tile.value && !lastTile.isDerived) {
-                                    collapse(tile, lastTile);
-                                }
-                            } else {
-                                pushToWall(tile, wall);
-                            }
-                        }
-                    }
-                }
-                break;
-            default:
-                console.log("Nothing to see here");
-        }
-        this.addNewTile()
-        return { occupied: this.coordinates, empty: this.emptyCoordinates };
+  moveWall(direction, wall) {
+    if (direction === 'up' || direction === 'left') {
+      return wall++
     }
+    return wall--
+  }
 
-    removeTile(coord) {
-        delete this.coordinates[coord];
-        this.emptyCoordinates[coord] = { key: coord, value: undefined };
-    }
+  isVertical(direction) {
+    return direction === 'up' || direction === 'down'
+  }
 
-    tallyscore() {
-        let keys = Object.keys(this.coordinates);
-        this.score = 0;
-        for (let i = 0; i < keys.length; i++) {
-            let tileKey = keys[i];
-            let tileValue = this.coordinates[tileKey].value;
-            this.score = this.score + tileValue;
-        }
+  wallIsStillInitial(wall, direction) {
+    return wall === this.getInitialWall(direction)
+  }
+
+  getLastTile(currentLine, wall, direction) {
+    if (this.wallIsStillInitial(wall, direction)) {
+      return null
     }
+    let coord
+    if (this.isVertical(direction)) {
+      coord = this.getCoord({row: wall, column: currentLine})
+    } else {
+      coord = this.getCoord({row: currentLine, column: wall})
+    }
+    return {
+      key: coord,
+      value: this.coordinates[coord],
+      isDerived: false,
+    }
+  }
+
+  collapse(sourceTile, destTile) {
+    this.removeTile(sourceTile.key)
+    this.coordinates[destTile.key] *= 2
+    this.score += this.coordinates[destTile.key]
+  }
+
+  shouldCollapse(tile, lastTile) {
+    return lastTile && lastTile.value === tile.value && !lastTile.isDerived
+  }
+
+  removeTile(coord) {
+    delete this.coordinates[coord]
+    this.emptyCoordinates[coord] = {key: coord, value: undefined}
+  }
+
+  isAtWall(tile, wall, direction) {
+    if (this.isVertical(direction)) {
+      return this.getRow(tile) === wall
+    } else {
+      return this.getColumn(tile) === wall
+    }
+  }
+
+  getRow(tile) {
+    return JSON.parse(tile.key).row
+  }
+
+  getColumn(tile) {
+    return JSON.parse(tile.key).column
+  }
+
+  pushToWall(tile, wall, direction) {
+    if (this.isAtWall(tile, wall, direction)) {
+      throw new Error('Tile is already at wall')
+    } else {
+      let coord
+      if (this.isVertical(direction)) {
+        coord = this.getCoord({row: wall, column: this.getColumn(tile)})
+      } else {
+        coord = this.getCoord({row: this.getRow(tile), column: wall})
+      }
+      this.coordinates[coord] = {key: coord, value: tile.value}
+      this.removeTile(tile)
+    }
+  }
+
+  getValue(i, j) {
+    const tile = this.coordinates[this.getCoord({row: i, column: j})]
+    if (!tile) {
+      return 0
+    }
+    return tile.value
+  }
 }
-
-class GameUI {
-    constructor(n, domId) {
-        this.size = n;
-        this.colorMap = [
-            { color: "black", bgColor: "white" },
-            { color: "blue", bgColor: "yellow" }
-        ];
-        this.board = new Board(this.size);
-        this.boardElement = document.getElementById(domId);
-        this.initEmptyBoardTiles();
-        this.attachEventHandler();
-    }
-
-    initEmptyBoardTiles() {
-        for (let i = 0; i < this.size; i++) {
-            for (let j = 0; j < this.size; j++) {
-                const coordinate = `${i},${j}`;
-                let tileElement = document.createElement("div");
-                tileElement.classList.add("tile");
-                tileElement.setAttribute("id", coordinate);
-                this.boardElement && this.boardElement.appendChild(tileElement);
-            }
-        }
-    }
-
-    attachEventHandler() {
-        this.boardElement &&
-            this.boardElement.addEventListener("keypress", this.handleKeypress);
-    }
-
-    handleKeypress(evt) {
-        switch (evt.keyCode) {
-            case 37:
-                this.move("left");
-                break;
-            case 38:
-                this.move("up");
-                break;
-            case 39:
-                this.move("right");
-                break;
-            case 40:
-                this.move("down");
-                break;
-            default:
-                console.log("You can move in only 4 directions");
-        }
-    }
-
-    move(direction) {
-        this.board.move(direction);
-        this.render();
-    }
-
-    render() { }
-
-    renderTile(tile) {
-        let tileElement = document.getElementById(tile.key);
-        tileElement.innerHTML = tile.value;
-    }
-}
-
-const gameUI = new GameUI(4, "game");
-
-const testBoard = new Board(4);
-console.log(testBoard.emptyCoordinates);
-console.log(testBoard.coordinates);
-console.log(testBoard.move("left"));
-console.log(testBoard.move("left"));
-console.log(testBoard.move("left"));
---
+++ main.js
@@ -0,0 +1,60 @@
+import {Board} from './index.js'
+
+class GameUI {
+  constructor(n, domId) {
+    this.size = n
+    this.board = new Board(this.size, () => {
+      alert('--game over--')
+    })
+    this.boardElement = document.getElementById(domId)
+    this.render()
+    this.handleKeypress = (evt) => {
+      switch (evt.keyCode) {
+        case 37:
+          this.move('left')
+          break
+        case 38:
+          this.move('up')
+          break
+        case 39:
+          this.move('right')
+          break
+        case 40:
+          this.move('down')
+          break
+        default:
+          console.log('You can move in only 4 directions')
+      }
+    }
+    this.attachEventHandler()
+  }
+
+  attachEventHandler() {
+    document.addEventListener('keydown', this.handleKeypress)
+  }
+
+  render(direction = '') {
+    const tableChild = (tag, i = 0) => {
+      let html = ''
+      for (let j = 0; j < this.size; j++) {
+        html = `${html}<${tag}>${
+          tag === 'tr' ? tableChild('td', j) : this.board.getValue(i, j)
+        }</${tag}>`
+      }
+      return html
+    }
+
+    const rows = tableChild('tr')
+    const table = `<table>${rows}</table>`
+    this.boardElement.innerHTML = `${
+      this.boardElement.innerHTML
+    }<br><br><div>${direction}<br>scrore: ${this.board.score}</div>${table}`
+  }
+
+  move(direction) {
+    this.board.move(direction)
+    this.render(direction)
+  }
+}
+
+new GameUI(4, 'game')
