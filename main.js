a75ad86ffc62dfffd0c9b1fca43d3fb3dd7a6dff
thecoffiner <33999780+thecoffiner@users.noreply.github.com>
2019-06-29 17:07:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,62 @@
 import { Board } from './index.js'
 
-let b = new Board(4)
+class GameUI {
+  constructor(n, domId) {
+    this.size = n
+    this.board = new Board(this.size, () => {
+      alert('--game over--')
+    })
+    this.boardElement = document.getElementById(domId)
+    this.render()
+    this.handleKeypress = evt => {
+      switch (evt.keyCode) {
+        case 37:
+          this.move('left')
+          break
+        case 38:
+          this.move('up')
+          break
+        case 39:
+          this.move('right')
+          break
+        case 40:
+          this.move('down')
+          break
+        default:
+          console.log('You can move in only 4 directions')
+      }
+    }
+    this.attachEventHandler()
+  }
 
-let d = document.getElementById('game')
-d.innerHTML = b.coordinates
+  attachEventHandler() {
+    document.addEventListener('keydown', this.handleKeypress)
+  }
+
+  render(direction = '') {
+    const tableChild = (tag, i = 0) => {
+      let html = ''
+      for (let j = 0; j < this.size; j++) {
+        html = `${html}<${tag}>${
+          tag === 'tr' ? tableChild('td', j) : this.board.getValue(i, j)
+        }</${tag}>`
+      }
+      return html
+    }
+
+    const rows = tableChild('tr')
+    const table = `<table>${rows}</table>`
+    this.boardElement.innerHTML = `<br><br><div>${direction}<br>score: ${
+      this.board.score
+    }</div>${table}`
+    window.scrollTo(0, document.body.scrollHeight)
+  }
+
+  move(direction) {
+    this.board.move(direction)
+
+    this.render(direction)
+  }
+}
+
+new GameUI(4, 'game')
