60575e9d84c6579c903103d6d8df4847a96a86c0
Solomon Omojola <omojolasolomon@gmail.com>
2019-07-01 22:40:58 +0200

    Full master code

--- main.js
+++ main.js
@@ -1,62 +1,62 @@
-import { Board } from './index.js'
+import { Board } from "./index.js";
 
 class GameUI {
   constructor(n, domId) {
-    this.size = n
+    this.size = n;
     this.board = new Board(this.size, () => {
-      alert('--game over--')
-    })
-    this.boardElement = document.getElementById(domId)
-    this.render()
+      alert("--game over--");
+    });
+    this.boardElement = document.getElementById(domId);
+    this.render();
     this.handleKeypress = evt => {
       switch (evt.keyCode) {
         case 37:
-          this.move('left')
-          break
+          this.move("left");
+          break;
         case 38:
-          this.move('up')
-          break
+          this.move("up");
+          break;
         case 39:
-          this.move('right')
-          break
+          this.move("right");
+          break;
         case 40:
-          this.move('down')
-          break
+          this.move("down");
+          break;
         default:
-          console.log('You can move in only 4 directions')
+          console.log("You can move in only 4 directions");
       }
-    }
-    this.attachEventHandler()
+    };
+    this.attachEventHandler();
   }
 
   attachEventHandler() {
-    document.addEventListener('keydown', this.handleKeypress)
+    document.addEventListener("keydown", this.handleKeypress);
   }
 
-  render(direction = '') {
+  render(direction = "") {
     const tableChild = (tag, i = 0) => {
-      let html = ''
+      let html = "";
       for (let j = 0; j < this.size; j++) {
         html = `${html}<${tag}>${
-          tag === 'tr' ? tableChild('td', j) : this.board.getValue(i, j)
-        }</${tag}>`
+          tag === "tr" ? tableChild("td", j) : this.board.getValue(i, j)
+        }</${tag}>`;
       }
-      return html
-    }
+      return html;
+    };
 
-    const rows = tableChild('tr')
-    const table = `<table>${rows}</table>`
+    const rows = tableChild("tr");
+    const table = `<table>${rows}</table>`;
     this.boardElement.innerHTML = `<br><br><div>${direction}<br>score: ${
       this.board.score
-    }</div>${table}`
-    window.scrollTo(0, document.body.scrollHeight)
+    }</div>${table}`;
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
   move(direction) {
-    this.board.move(direction)
+    this.board.move(direction);
 
-    this.render(direction)
+    this.render(direction);
   }
 }
 
-new GameUI(4, 'game')
+new GameUI(4, "game");
